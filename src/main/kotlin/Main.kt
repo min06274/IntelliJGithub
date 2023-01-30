c56787363425d6fa7a3fa773f5fe289b64211af7
Eddy <min06274@naver.com>
2023-01-30 19:53:12 +0900

    실습 2 isNullOrBlank() 사용예시 카테고리에 없는 것 받아도 다시 입력 받기 when사용

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,48 @@ fun main(args: Array<String>) {
         원하시는 카테고리를 입력해주세요.
         ***=============================***
     """.trimIndent())
+
+    val categories = arrayOf("패션","전자기기","반려동물용품")
+    for(category in categories)
+    {
+        println(category)
+    }
+
+    var ch: String? = null
+
+
+    while(true) {
+        ch = readLine()
+        when {
+            !ch.isNullOrBlank() -> {
+                when {
+                    ch == "패션" -> {
+                        println("패션")
+                        break
+                    }
+
+                    ch == "전자기기" -> {
+                        println("전자기기")
+                        break
+                    }
+
+                    ch == "반려동물용품" -> {
+                        println("반려동물용품")
+                        break
+                    }
+                    else ->{
+                        println("카테고리에서 다시 입력해주세요")
+                        continue
+                    }
+                }
+            }
+            ch.isNullOrBlank()->{
+                println("공백은 입력이 안됩니다 다시 입력해주세요")
+                continue
+            }
+
+
+        }
+
+    }
 }
