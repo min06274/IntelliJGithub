39761d826075502876b739a39bab286e1f7931b0
Eddy <min06274@naver.com>
2023-01-31 21:25:32 +0900

    실습 3 클래스 나누기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,8 @@
+import screen.ShoppingHome
+
 fun main(args: Array<String>) {
 
-    println("안녕하세요, Shoppi 에 오신 것을 환영합니다!")
-    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
+    val shoppingHome = ShoppingHome()
 
-    val name = readLine()
-
-    println("""
-        감사합니다. 반갑습니다, ${name}님
-        원하시는 카테고리를 입력해주세요.
-        ***=============================***
-    """.trimIndent())
-
-    val categories = arrayOf("패션","전자기기","반려동물용품")
-    for(category in categories)
-    {
-        println(category)
-    }
-
-    var ch: String? = null
-
-
-    while(true) {
-        ch = readLine()
-        when {
-            !ch.isNullOrBlank() -> {
-                when {
-                    ch == "패션" -> {
-                        println("패션")
-                        break
-                    }
-
-                    ch == "전자기기" -> {
-                        println("전자기기")
-                        break
-                    }
-
-                    ch == "반려동물용품" -> {
-                        println("반려동물용품")
-                        break
-                    }
-                    else ->{
-                        println("카테고리에서 다시 입력해주세요")
-                        continue
-                    }
-                }
-            }
-            ch.isNullOrBlank()->{
-                println("공백은 입력이 안됩니다 다시 입력해주세요")
-                continue
-            }
-
-
-        }
-
-    }
+    shoppingHome.start()
 }
